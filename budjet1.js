a7207b757f5e9b2e023794541fe4a4d909588ca0
Dulat <hupopop@gmail.com>
2021-03-29 14:49:41 +0600

    first commit

---
+++ budjet1.js
@@ -0,0 +1,38 @@
+'use strict';
+let money = prompt("Ваш бюджет на месяц?", ''),
+time = prompt("Введите дату в формате YYYY-MM-DD", '');
+
+let appData = {
+    budget: money,
+    expenses: {},
+    optionalExpenses: {},
+    income: [],
+    timedata: time,
+    savinges: false
+};
+
+appData.expenses.a1 = a2;
+appData.expenses.a3 = a4;
+
+alert(appData.budget / 30);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
